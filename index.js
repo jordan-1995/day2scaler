cf9043029e2666c0e4c5818cafff8bd85333d456
Akshay More <79933068+jordan-1995@users.noreply.github.com>
2025-06-10 00:54:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let cleanString = s.toLowerCase();
+    let finalString = cleanString.replace(/[^a-z]/g, "")
+    let reversedString = finalString.split("").reverse().join("");
+    if (finalString == reversedString){
+        return true;
+    } else if(finalString != reversedString){
+        return false;
+    } else if(finalString==null){
+        return true;
+    }
+};
